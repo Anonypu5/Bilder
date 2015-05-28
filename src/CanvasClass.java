ca794b3fa92d532fb9e96b759e3030d1f3c3e900
Anonypu5 <elias.fyksen@grabit.no>
2015-05-29 01:24:56 +0200

    Shit

---
+++ src/CanvasClass.java
@@ -0,0 +1,102 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+/**
+ * Created by Elias on 5/28/2015.
+ */
+public class CanvasClass extends Canvas {
+
+    public BufferedImage image;
+    public JFrame jf;
+    public BufferedImage screen;
+    public int[] screenArray;
+    public int[] imageArray;
+    public Operator[] ops;
+
+    public CanvasClass(BufferedImage bi){
+        image = bi;
+        screen = new BufferedImage(image.getWidth(),image.getHeight(),BufferedImage.TYPE_INT_RGB);
+        screenArray = ((DataBufferInt)screen.getRaster().getDataBuffer()).getData();
+        imageArray = image.getRGB(0,0,image.getWidth(),image.getHeight(),null,0,image.getWidth());
+
+        setSize(image.getWidth(),image.getHeight());
+
+        jf = new JFrame("bilde");
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jf.setResizable(false);
+        jf.add(this);
+        jf.pack();
+        jf.setLocationRelativeTo(null);
+        jf.setVisible(true);
+    }
+
+    public void render(){
+        BufferStrategy bs = getBufferStrategy();
+        if (bs== null) {
+            createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        g.drawImage(screen, 0, 0, null);
+        g.dispose();
+        bs.show();
+    }
+
+    public void update(){
+        for(int i = 0; i < screenArray.length; i++){
+            screenArray[i] = 0x000000;
+        }
+        for(int i = 0; i < ops.length; i++){
+            screenArray = ops[i].operation(imageArray,screenArray);
+        }
+    }
+}class Operator{
+
+    int vr=0,vg=0,vb=0,
+            lr=0,lg=0,lb=0;
+
+    public int[] operation(int[] source, int[] image){
+        for(int i = 0; i < source.length; i++){
+
+            int hex = source[i];
+            int r = (hex & 0xFF0000) >> 16;
+            int g = (hex & 0xFF00) >> 8;
+            int b = (hex & 0xFF);
+            int nr=0,ng=0,nb=0;
+
+            if(r > lr){
+                nr = vr;
+            }
+            if(g > lg){
+                ng = vg;
+            }
+            if(b > lb){
+                nb = vb;
+            }
+
+            int hex2 = image[i];
+            int r2 = (hex2 & 0xFF0000) >> 16;
+            int g2 = (hex2 & 0xFF00) >> 8;
+            int b2 = (hex2 & 0xFF);
+
+            r2 += nr;
+            b2 += nb;
+            g2 += ng;
+
+            if(r2 > 255)
+                r2 = 255;
+            if(b2 > 255)
+                b2 = 255;
+            if(g2 > 255)
+                g2 = 255;
+
+            image[i] = (r2<<16) + (g2<<8) + b2;
+        }
+
+        return image;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Elias on 5/28/2015.
+ */
+public class Main {
+    public Main(CanvasClass canvas) {
+        canvas.ops = new Operator[1];
+        canvas.ops[0] = new Operator();
+        canvas.ops[0].lr = 105;
+        canvas.ops[0].vr = 255;
+        canvas.ops[0].lg = 150;
+        canvas.ops[0].vg = 255;
+        canvas.ops[0].lb = 100;
+        canvas.ops[0].vb = 255;
+        long time = System.currentTimeMillis();
+        long counter = 0;
+        while (true){
+            long deltaTime = System.currentTimeMillis()-time;
+            time += deltaTime;
+            counter += deltaTime;
+            float r = (float)(counter)/1000;
+            canvas.ops[0].lr = (int)(Math.sin(r)*100+100);
+            float g = (float)(counter)/1000*0.5f;
+            canvas.ops[0].lg = (int)(Math.sin(g)*100+100);
+            float b = (float)(counter)/1000*2;
+            canvas.ops[0].lb = (int)(Math.sin(b)*100+100);
+            canvas.render();
+            canvas.update();
+        }
+    }
+
+    public static void main(String[] args) {
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("","jpg", "png");
+        chooser.setFileFilter(filter);
+        chooser.showOpenDialog(null);
+        File file = chooser.getSelectedFile();
+        BufferedImage bi = null;
+        try {
+            bi = ImageIO.read(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        CanvasClass canvas = new CanvasClass(bi);
+
+        new Main(canvas);
+    }
+}
